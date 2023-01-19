fbb027e3f358dad0394ab3688c2bf89b40bc7994
Your Name <jokarkuii@gmail.com>
2023-01-19 14:39:14 +0900

    Player,Main更新

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
+    // monster = new Monster();
+    Player player = new Player();
+
+    player.drawMonsters();
+    player.showDeck();
+
+    // System.out.println(monster.toString());
   }
 }
